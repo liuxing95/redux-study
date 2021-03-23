268209c88d53a74433f032ef53bfb74f52746f33
liuxing95 <1358974006>
2021-03-23 10:36:12 +0800

    feat: add applyMiddleware

---
+++ src/types/middleware.ts
@@ -0,0 +1,16 @@
+import { Dispatch } from "./store";
+
+export interface MiddlewareAPI<D extends Dispatch = Dispatch, S = any> {
+  dispatch: D
+  getState(): S
+}
+
+export interface Middleware<
+  _DispatchExt = {},
+  S = any,
+  D extends Dispatch = Dispatch
+> {
+  (api: MiddlewareAPI<D, S>): (
+    next: D
+  ) => (action: D extends Dispatch<infer A> ? A : never) => any
+}
